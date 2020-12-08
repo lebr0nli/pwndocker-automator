145ef0423216ca7288e71763a59528d6661b6778
lebr0nli <alan.li.tw@gmail.com>
2020-12-08 14:42:30 +0800

    update linux shell script

--- startup.sh
+++ startup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 if docker info | grep -q 'daemon'; then
-   open /Applications/Docker.app
+   echo 'please start your docker daemon first...'
 fi
 while docker info | grep -q 'daemon';
 do
-	sleep 3
+	echo 'wating your docker daemon start...'
+    sleep 3
 done
 cd ~/Desktop && /usr/local/bin/docker run -d --rm -h pwndocker --name pwndocker -v $(pwd)/pwndocker:/ctf/work -p 23946:23946 --cap-add=SYS_PTRACE skysider/pwndocker
 clear;docker exec -it pwndocker /bin/bash
